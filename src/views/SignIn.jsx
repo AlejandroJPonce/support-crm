3ff2a3dddeb5c565b6e0128bda4be32424f71818
Alejandro Ponce <alejandroponce@192.168.1.9>
2024-08-30 01:51:26 -0500

    feat: fix input size

--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -5,8 +5,8 @@ export default function SignIn () {
   <>
     <div className='signin-form-box'>
       <span className='box-title'>Ingresar</span>
-      <input className='form-input' placeholder='Email'></input>
-      <input className='form-input' placeholder='Password'></input>
+      <input className='form-input' placeholder='Email' type='email'></input>
+      <input className='form-input' placeholder='Password' type='password'></input>
       <a href="/sign-up" className='form-route'>Aun no haces parte del equipo ?</a>
       <button className='login-button'>Iniciar Sesion</button>
     </div>
--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -5,8 +5,8 @@ export default function SignUp () {
   <>
     <div className='signin-form-box'>
       <span className='box-title'>Ingresar</span>
-      <input className='form-input' placeholder='Email'></input>
-      <input className='form-input' placeholder='Password'></input>
+      <input className='form-input' placeholder='Email' type='email'></input>
+      <input className='form-input' placeholder='Password' type='password'></input>
       <a href="/" className='form-route'>Ir a iniciar sesion</a>
       <button className='login-button'>Registrarme</button>
     </div>
